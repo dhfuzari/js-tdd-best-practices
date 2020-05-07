a76284ab8df08275ae625be6a416edd4c53053b0
Daniel H. Fuzari <daniel.fuzari@i-value.com.br>
2020-05-06 22:42:37 -0300

    feat(arrow functions scope): arrow functions scope and template literals

---
+++ src/secao2-aprendendo-es6/arrow-function.js
@@ -0,0 +1,14 @@
+const ireland = ["Dublin", "Galway", "Cork"];
+
+const love = ireland.map(function (name) {
+  return `I love ${name}`;
+});
+console.log(love);
+
+const loveArrowFunc = ireland.map((name) => `I love ${name}`);
+console.log(loveArrowFunc);
+
+const loveArrowFuncFilter = ireland
+  .filter((name) => name === "Dublin")
+  .map((name) => `I love ${name}`);
+console.log(loveArrowFuncFilter);
