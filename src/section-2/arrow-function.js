871127f7f655769809202faab8e775bfa45ef9a9
Daniel H. Fuzari <dhfuzari@gmail.com>
2020-12-26 23:39:52 -0300

    style(src): fixed eslint issues

--- src/section-2/arrow-function.js
+++ src/section-2/arrow-function.js
@@ -1,12 +1,12 @@
 const ireland = ['Dublin', 'Galway', 'Cork'];
 
-const love = ireland.map(name => `I love ${name}`);
+const love = ireland.map((name) => `I love ${name}`);
 console.log(love);
 
-const loveArrowFunc = ireland.map(name => `I love ${name}`);
+const loveArrowFunc = ireland.map((name) => `I love ${name}`);
 console.log(loveArrowFunc);
 
 const loveArrowFuncFilter = ireland
-  .filter(name => name === 'Dublin')
-  .map(name => `I love ${name}`);
+  .filter((name) => name === 'Dublin')
+  .map((name) => `I love ${name}`);
 console.log(loveArrowFuncFilter);
