4b7dc4778797e0ec721a21dd76fd1f817da25d94
Daniel H. Fuzari <dhfuzari@gmail.com>
2020-12-29 16:22:10 -0300

    feat(section-2): javascript symbols

---
+++ src/section-2/symbols.js
@@ -0,0 +1,29 @@
+// A primtive type, like an array or object
+
+const foo = Symbol('name');
+const bar = Symbol('name');
+
+console.log(foo, typeof foo);
+console.log(bar, typeof bar);
+console.log(foo === bar);
+
+const person = {
+  [Symbol('city')]: 'Esp. Sto. do Pinhal',
+  [Symbol('name')]: 'Daniel',
+  [Symbol('name')]: 'Henrique',
+  age: 36,
+};
+
+console.log(person);
+console.log(Object.keys(person));
+console.log(Object.getOwnPropertySymbols(person));
+
+const symbolsOfPerson = Object.getOwnPropertySymbols(person);
+console.log(symbolsOfPerson);
+
+const dataSymbols = symbolsOfPerson.map((symbol) => {
+  console.log(symbol, person[symbol]);
+  return person[symbol];
+});
+
+console.log(dataSymbols);
