8d9c2eb260abfbdafb799044216ffcf767a6ef70
Daniel H. Fuzari <dhfuzari@gmail.com>
2020-12-29 14:55:57 -0300

    feat(section-2): extended classes

---
+++ src/section-2/extended-classes.js
@@ -0,0 +1,29 @@
+/* eslint-disable max-classes-per-file */
+
+class Animal {
+  constructor(kind) {
+    this.kind = kind;
+  }
+
+  hello() {
+    console.log(`Hi, I'm a(n) ${this.kind}`);
+  }
+}
+
+const elephant = new Animal('Elephant');
+elephant.hello();
+
+class Dog extends Animal {
+  constructor(kind, sound) {
+    super(kind);
+    this.sound = sound;
+  }
+
+  bark() {
+    console.log(`${this.sound} I'm ${this.kind}`);
+  }
+}
+
+const dog = new Dog('Dog', 'Au-au');
+dog.hello();
+dog.bark();
