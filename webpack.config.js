1748606eec0b0c4f5b61095466fa8150bf78363d
Daniel H. Fuzari <dhfuzari@gmail.com>
2021-01-27 12:37:22 -0300

    chore: config the new way to use babel and webpack with preset-env

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    filename: "./src/section-4/webpack-modules/app.js",
+  },
+  output: {
+    filename: "build.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
